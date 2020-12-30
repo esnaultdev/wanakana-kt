c97e202b0e3014bebfa5c0727f6af979d536c1a1
Matthieu Esnault <matthieu.esn0@gmail.com>
2020-12-30 21:44:55 +0900

    Add a skeleton of the public interface

---
+++ wanakana/src/main/java/dev/esnault/wanakana/Wanakana.kt
@@ -0,0 +1,29 @@
+package dev.esnault.wanakana
+
+
+object Wanakana {
+
+    // IME event listener DOM helpers
+    // TODO This should be done in a separate Android library
+    // bind
+    // unbind
+
+    // Writing system checks
+    // isRomaji
+    // TODO isJapanese
+    // TODO isKana
+    // TODO isHiragana
+    // TODO isKatakana
+    // isMixed
+    // isKanji
+
+    // Conversion
+    // toRomaji
+    // toKana
+    // TODO toHiragana
+    // TODO toKatakana
+
+    // Other utils
+    // TODO stripOkurigana
+    // TODO tokenize
+}
