c42fa09090ff55397c25f30b125dbefde554578d
Matthieu Esnault <matthieu.esn0@gmail.com>
2021-01-04 18:35:12 +0900

    Add tokenize

---
+++ wanakana/src/main/java/dev/esnault/wanakana/Tokenize.kt
@@ -0,0 +1,167 @@
+package dev.esnault.wanakana
+
+import dev.esnault.wanakana.utils.isEnglishPunctuation
+import dev.esnault.wanakana.utils.isHiragana
+import dev.esnault.wanakana.utils.isJapanese
+import dev.esnault.wanakana.utils.isJapanesePunctuation
+import dev.esnault.wanakana.utils.isKanji
+import dev.esnault.wanakana.utils.isKatakana
+import dev.esnault.wanakana.utils.isRomaji
+
+/**
+ * Type of text token.
+ */
+enum class TokenType(val value: String) {
+    EN("en"),
+    JA("ja"),
+    EN_NUM("englishNumeral"),
+    JA_NUM("japaneseNumeral"),
+    EN_PUNC("englishPunctuation"),
+    JA_PUNC("japanesePunctuation"),
+    KANJI("kanji"),
+    HIRAGANA("hiragana"),
+    KATAKANA("katakana"),
+    SPACE("space"),
+    OTHER("other")
+}
+
+/**
+ * A text token [value] with its associated [type].
+ */
+data class TypedToken(
+    val value: String,
+    val type: TokenType
+)
+
+/**
+ * Splits input into a list of strings separated by opinionated [TokenType]s.
+ * @param input the text to tokenize.
+ * @param compact if `true`, then many same-language tokens are combined (spaces + text, kanji +
+ * kana, numeral + punctuation). Defaults to `false`.
+ * @return the text split into [String] tokens.
+ *
+ * For example:
+ * - `tokenize("ふふフフ")` => `["ふふ", "フフ"]`
+ * - `tokenize("感じ")` => `["感", "じ"]`
+ * - `tokenize("truly 私は悲しい")` => ["truly", " ", "私", "は", "悲", "しい"]`
+ * - `tokenize("truly 私は悲しい", compact = true)` => ["truly ", "私は悲しい"]`
+ * - `tokenize("5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！")`
+ *   => `[ "5", "romaji", " ", "here", "...!?", "漢字", "ひらがな", "カタ", "　", "カナ", "４", "「", "ＳＨＩＯ", "」。！"]`
+ * - `tokenize("5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！", compact = true)`
+ *   => `[ "5", "romaji here", "...!?", "漢字ひらがなカタ　カナ", "４「", "ＳＨＩＯ", "」。！"]`
+ */
+fun tokenize(input: String, compact: Boolean = false): List<String> =
+    tokenizeWithType(input, compact).map(TypedToken::value)
+
+/**
+ * Splits input into a list of tokens separated by opinionated [TokenType]s.
+ * @param input the text to tokenize.
+ * @param compact if `true`, then many same-language tokens are combined (spaces + text, kanji +
+ * kana, numeral + punctuation). Defaults to `false`.
+ * @return the text split into [TypedToken]s.
+ *
+ * For example:
+ * - `tokenize('5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！ لنذهب')` =>
+ * ```
+ * [
+ *  { type: 'englishNumeral', value: '5' },
+ *  { type: 'en', value: 'romaji' },
+ *  { type: 'space', value: ' ' },
+ *  { type: 'en', value: 'here' },
+ *  { type: 'englishPunctuation', value: '...!?' },
+ *  { type: 'kanji', value: '漢字' },
+ *  { type: 'hiragana', value: 'ひらがな' },
+ *  { type: 'katakana', value: 'カタ' },
+ *  { type: 'space', value: '　' },
+ *  { type: 'katakana', value: 'カナ' },
+ *  { type: 'japaneseNumeral', value: '４' },
+ *  { type: 'japanesePunctuation', value: '「' },
+ *  { type: 'ja', value: 'ＳＨＩＯ' },
+ *  { type: 'japanesePunctuation', value: '」。！' },
+ *  { type: 'space', value: ' ' },
+ *  { type: 'other', value: 'لنذهب' }
+ * ]
+ * ```
+ *
+ * - `tokenize('5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！ لنذهب', compact = true)` =>
+ * ```
+ * [
+ *  { type: 'other', value: '5' },
+ *  { type: 'en', value: 'romaji here' },
+ *  { type: 'other', value: '...!?' },
+ *  { type: 'ja', value: '漢字ひらがなカタ　カナ' },
+ *  { type: 'other', value: '４「' },
+ *  { type: 'ja', value: 'ＳＨＩＯ' },
+ *  { type: 'other', value: '」。！' },
+ *  { type: 'en', value: ' ' },
+ *  { type: 'other', value: 'لنذهب' }
+ * ]
+ * ```
+ */
+fun tokenizeWithType(input: String, compact: Boolean = false): List<TypedToken> {
+    if (input.isEmpty()) return emptyList()
+
+    var currentValue: String
+    var currentType: TokenType
+
+    // Initialize the current state using the first char
+    input.first().let { firstChar ->
+        currentValue = firstChar.toString()
+        currentType = getType(firstChar, compact)
+    }
+
+    val result = mutableListOf<TypedToken>()
+    // Iterate through the chars, concatenating same types
+    input.drop(1).forEach { char ->
+        val type = getType(char, compact)
+        if (type == currentType) {
+            currentValue += char
+        } else {
+            result.add(TypedToken(currentValue, currentType))
+            currentValue = char.toString()
+            currentType = type
+        }
+    }
+    // Don't forget to add the current token.
+    result.add(TypedToken(currentValue, currentType))
+    return result
+}
+
+private fun getType(input: Char, compact: Boolean = false): TokenType {
+    return if (compact) {
+        when {
+            input.isJaNum() -> TokenType.OTHER
+            input.isEnNum() -> TokenType.OTHER
+            input.isEnSpace() -> TokenType.EN
+            input.isEnglishPunctuation() -> TokenType.OTHER
+            input.isJaSpace() -> TokenType.JA
+            input.isJapanesePunctuation() -> TokenType.OTHER
+            input.isJapanese() -> TokenType.JA
+            input.isRomaji() -> TokenType.EN
+            else -> TokenType.OTHER
+        }
+    } else {
+        when {
+            input.isJaSpace() -> TokenType.SPACE
+            input.isEnSpace() -> TokenType.SPACE
+            input.isJaNum() -> TokenType.JA_NUM
+            input.isEnNum() -> TokenType.EN_NUM
+            input.isJapanesePunctuation() -> TokenType.JA_PUNC
+            input.isEnglishPunctuation() -> TokenType.EN_PUNC
+            input.isKanji() -> TokenType.KANJI
+            input.isHiragana() -> TokenType.HIRAGANA
+            input.isKatakana() -> TokenType.KATAKANA
+            input.isJapanese() -> TokenType.JA
+            input.isRomaji() -> TokenType.EN
+            else -> TokenType.OTHER
+        }
+    }
+}
+
+private fun Char.isEnSpace(): Boolean = this == ' '
+private fun Char.isJaSpace(): Boolean = this == '　'
+private fun Char.isJaNum(): Boolean = jaNumRegex.matches(this.toString())
+private fun Char.isEnNum(): Boolean = enNumRegex.matches(this.toString())
+
+private val jaNumRegex: Regex by lazy(LazyThreadSafetyMode.NONE) { Regex("[０-９]") }
+private val enNumRegex: Regex by lazy(LazyThreadSafetyMode.NONE) { Regex("[0-9]") }
---
+++ wanakana/src/test/java/dev/esnault/wanakana/core/TokenizeTest.kt
@@ -0,0 +1,120 @@
+package dev.esnault.wanakana.core
+
+import dev.esnault.wanakana.DynamicTestsBuilder
+import dev.esnault.wanakana.TokenType
+import dev.esnault.wanakana.TypedToken
+import dev.esnault.wanakana.dynamicTests
+import dev.esnault.wanakana.tokenize
+import dev.esnault.wanakana.tokenizeWithType
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import kotlin.test.assertEquals
+
+@DisplayName("tokenize()")
+class TokenizeTest {
+
+    fun DynamicTestsBuilder.test(name: String, input: String, expected: List<String>) =
+        testEquals(name = name, expected = expected) { tokenize(input = input) }
+
+    @TestFactory
+    @DisplayName("Basic usage")
+    fun basicUsageTest() = dynamicTests {
+        test(name = "Empty", input = "", expected = emptyList())
+        test(name = "ふふ", input = "ふふ", expected = listOf("ふふ"))
+        test(name = "フフ", input = "フフ", expected = listOf("フフ"))
+        test(name = "ふふフフ", input = "ふふフフ", expected = listOf("ふふ", "フフ"))
+        test(name = "阮咸", input = "阮咸", expected = listOf("阮咸"))
+        test(name = "感じ", input = "感じ", expected = listOf("感", "じ"))
+        test(name = "私は悲しい", input = "私は悲しい", expected = listOf("私", "は", "悲", "しい"))
+        test(name = "ok لنذهب!", input = "ok لنذهب!", expected = listOf("ok", " ", "لنذهب", "!"))
+    }
+    
+    @Test
+    @DisplayName("Handles mixed input")
+    fun mixedInputTest() {
+        val input = "5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！"
+        val tokenized = tokenize(input)
+        val expected = listOf(
+            "5",
+            "romaji",
+            " ",
+            "here",
+            "...!?",
+            "漢字",
+            "ひらがな",
+            "カタ",
+            "　",
+            "カナ",
+            "４",
+            "「",
+            "ＳＨＩＯ",
+            "」。！"
+        )
+        assertEquals(expected = expected, actual = tokenized)
+    }
+
+    @Test
+    @DisplayName("compact = true")
+    fun compactTest() {
+        val input = "5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！ لنذهب"
+        val tokenized = tokenize(input, compact = true)
+        val expected = listOf(
+            "5",
+            "romaji here",
+            "...!?",
+            "漢字ひらがなカタ　カナ",
+            "４「",
+            "ＳＨＩＯ",
+            "」。！",
+            " ",
+            "لنذهب"
+        )
+        assertEquals(expected = expected, actual = tokenized)
+    }
+
+    @Test
+    @DisplayName("tokenizeWithType()")
+    fun withTypeTest() {
+        val input = "5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！ لنذهب"
+        val tokenized = tokenizeWithType(input)
+        val expected = listOf(
+            TypedToken(type = TokenType.EN_NUM, value = "5"),
+            TypedToken(type = TokenType.EN, value = "romaji"),
+            TypedToken(type = TokenType.SPACE, value = " "),
+            TypedToken(type = TokenType.EN, value = "here"),
+            TypedToken(type = TokenType.EN_PUNC, value = "...!?"),
+            TypedToken(type = TokenType.KANJI, value = "漢字"),
+            TypedToken(type = TokenType.HIRAGANA, value = "ひらがな"),
+            TypedToken(type = TokenType.KATAKANA, value = "カタ"),
+            TypedToken(type = TokenType.SPACE, value = "　"),
+            TypedToken(type = TokenType.KATAKANA, value = "カナ"),
+            TypedToken(type = TokenType.JA_NUM, value = "４"),
+            TypedToken(type = TokenType.JA_PUNC, value = "「"),
+            TypedToken(type = TokenType.JA, value = "ＳＨＩＯ"),
+            TypedToken(type = TokenType.JA_PUNC, value = "」。！"),
+            TypedToken(type = TokenType.SPACE, value = " "),
+            TypedToken(type = TokenType.OTHER, value = "لنذهب")
+        )
+        assertEquals(expected = expected, actual = tokenized)
+    }
+
+    @Test
+    @DisplayName("tokenizeWithType(compact = true)")
+    fun withTypeAndCompactTest() {
+        val input = "5romaji here...!?漢字ひらがなカタ　カナ４「ＳＨＩＯ」。！ لنذهب"
+        val tokenized = tokenizeWithType(input, compact = true)
+        val expected = listOf(
+            TypedToken(type = TokenType.OTHER, value = "5"),
+            TypedToken(type = TokenType.EN, value = "romaji here"),
+            TypedToken(type = TokenType.OTHER, value = "...!?"),
+            TypedToken(type = TokenType.JA, value = "漢字ひらがなカタ　カナ"),
+            TypedToken(type = TokenType.OTHER, value = "４「"),
+            TypedToken(type = TokenType.JA, value = "ＳＨＩＯ"),
+            TypedToken(type = TokenType.OTHER, value = "」。！"),
+            TypedToken(type = TokenType.EN, value = " "),
+            TypedToken(type = TokenType.OTHER, value = "لنذهب")
+        )
+        assertEquals(expected = expected, actual = tokenized)
+    }
+}
